5b6023a943fc3169e8a33b3ca763ff0024350e1d
naira <nairacoppari@gmail.com>
2021-12-06 21:38:30 -0300

    agregando los primeros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { NavbarComponent } from './vistas/navbar/navbar.component';
+import { InputsComponent } from './componentes/inputs/inputs.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    InputsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatCardModule,
+    MatInputModule, 
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
