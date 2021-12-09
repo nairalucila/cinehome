f9605d45035282a8f335d3bc3e6545962134a5f3
Naira Coppari <nairacoppari@gmail.com>
2021-12-09 13:05:07 -0300

    problemas con el login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { NgModule,  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './componentes/login/login.component';
-import { NavbarComponent } from './vistas/navbar/navbar.component';
-import { InputsComponent } from './componentes/inputs/inputs.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatMenuModule } from '@angular/material/menu';
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { ListadoComponent } from './componentes/listado/listado.component';
 import { CarritoComponent } from './componentes/carrito/carrito.component';
-import {MatButtonModule} from '@angular/material/button';
+import { InputsComponent } from './componentes/inputs/inputs.component';
+import { ListadoComponent } from './componentes/listado/listado.component';
+import { LoginComponent } from './componentes/login/login.component';
 import { HomeComponent } from './vistas/home/home.component';
-import {MatTableModule} from '@angular/material/table';
-import {MatDividerModule} from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './vistas/navbar/navbar.component';
 
 @NgModule({
   declarations: [
