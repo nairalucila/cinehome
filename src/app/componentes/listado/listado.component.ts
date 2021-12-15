ad84082e7ca05c62857f23671a7f81aac0f430d5
Naira Coppari <nairacoppari@gmail.com>
2021-12-15 20:33:25 -0300

    se agregaron life hooks

--- src/app/componentes/listado/listado.component.ts
+++ src/app/componentes/listado/listado.component.ts
@@ -22,89 +22,25 @@ interface PeliculaSeleccionada {
 })
 export class ListadoComponent implements OnInit {
 
-  listaPeliculas: Peliculas[];
+  listaPeliculas: Peliculas[] = [];
   cantidadPeliculas: number;
   peliculaSeleccionadas: PeliculaSeleccionada[];
   algo: string;
 
   constructor(private mockapi: MockapiService) {
     this.algo = "hola"
-   this.traerPeliculaService();
     this.peliculaSeleccionadas = [];
     this.cantidadPeliculas = 0;
-    this.listaPeliculas = [{
-      titulo: "Frozen, el reino del Hielo",
-      genero: "animado",
-      año: 2011,
-      stock: 115,
-      precio: 200,
-      img: "https://www.caratulas.com/peliculas/peliculas/H/Hara_Kiri_Muerte_De_Un_Samurai/Hara_Kiri_Muerte_De_Un_Samurai-Cartel.jpg"
-    },
-    {
-      titulo: "Habemus Papam",
-      genero: "documental",
-      año: 2019,
-      stock: 115,
-      precio: 200,
-      img: "https://www.caratulas.com/peliculas/peliculas/H/Hara_Kiri_Muerte_De_Un_Samurai/Hara_Kiri_Muerte_De_Un_Samurai-Cartel.jpg"
-    },
-    {
-      titulo: "Hellboy",
-      genero: "comedia",
-      año: 2011,
-      stock: 115,
-      precio: 200,
-      img: "https://www.caratulas.com/peliculas/peliculas/H/Hara_Kiri_Muerte_De_Un_Samurai/Hara_Kiri_Muerte_De_Un_Samurai-Cartel.jpg"
-    },
-    {
-      titulo: "Frozen, el reino del Hielo",
-      genero: "animado",
-      año: 2011,
-      stock: 115,
-      precio: 200,
-      img: "https://www.caratulas.com/peliculas/peliculas/H/Hara_Kiri_Muerte_De_Un_Samurai/Hara_Kiri_Muerte_De_Un_Samurai-Cartel.jpg"
-    },
-    {
-      titulo: "Frozen, el reino del Hielo",
-      genero: "animado",
-      año: 2011,
-      stock: 115,
-      precio: 200,
-      img: "https://www.caratulas.com/peliculas/peliculas/H/Hara_Kiri_Muerte_De_Un_Samurai/Hara_Kiri_Muerte_De_Un_Samurai-Cartel.jpg"
-    },
-    {
-      titulo: "Frozen, el reino del Hielo",
-      genero: "animado",
-      año: 2011,
-      stock: 115,
-      precio: 200,
-      img: "https://www.caratulas.com/peliculas/peliculas/H/Hara_Kiri_Muerte_De_Un_Samurai/Hara_Kiri_Muerte_De_Un_Samurai-Cartel.jpg"
-    },
-    {
-      titulo: "Frozen, el reino del Hielo",
-      genero: "animado",
-      año: 2011,
-      stock: 115,
-      precio: 200,
-      img: "https://www.caratulas.com/peliculas/peliculas/H/Hara_Kiri_Muerte_De_Un_Samurai/Hara_Kiri_Muerte_De_Un_Samurai-Cartel.jpg"
-    },
-    {
-      titulo: "Frozen, el reino del Hielo",
-      genero: "animado",
-      año: 2011,
-      stock: 115,
-      precio: 200,
-      img: "https://www.caratulas.com/peliculas/peliculas/H/Hara_Kiri_Muerte_De_Un_Samurai/Hara_Kiri_Muerte_De_Un_Samurai-Cartel.jpg"
-    },
-    ]
+    
   }
 
   ngOnInit(): void {
+    this.traerPeliculaService();
   }
 
 traerPeliculaService(){
   this.mockapi.obtenerPeliculas().subscribe((pelis: any)=>{
-    console.log(pelis);
+    return this.listaPeliculas = pelis;
   })
 }
 
