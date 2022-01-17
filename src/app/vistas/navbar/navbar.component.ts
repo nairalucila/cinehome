86046404c07f508191a581c54bfb7037bb506732
nairalucila <nairacoppari@gmail.com>
2022-01-17 10:41:12 -0300

    cambios en navbar

--- src/app/vistas/navbar/navbar.component.ts
+++ src/app/vistas/navbar/navbar.component.ts
@@ -1,25 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap, ActivatedRouteSnapshot, NavigationEnd } from '@angular/router';
 
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { 
-    
+  mostrarNav: boolean;
+  currentRoute: string = "";
+  constructor(private route: Router) {
+    this.mostrarNav = true;
   }
 
   ngOnInit(): void {
+    this.ocultarBarraNavegacion();
+
+    
   }
-  //Se tiene pensado que se abra como modal
-  
-  // openDialog() {
-  //   this.dialog.open(CarritoComponent);
-  // }
 
+  ocultarBarraNavegacion(){
+    // this.route.config.forEach(rutas => {
+    //   if(rutas.path === "login" || rutas.path === "registro"){
+    //     this.mostrarNav = false;
+    //   }
+    // });
+  };
 }
-
-
